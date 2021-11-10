73e4909ab227d097358e1b1dc93dc65dc78178cc
Safwat Musthafa <T00543609@mytru.ca>
2021-11-10 13:52:19 -0800

    hello

--- home.js
+++ home.js
@@ -51,21 +51,19 @@ function f9() {
 
 
 function start() {
-webgazer.setGazeListener(function(data, elapsedTime) {
-    if (data == null) {
-        return;
-    }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(elapsedTime); //elapsed time is based on time since begin was called
-}).begin();
+	webgazer.setGazeListener(function(data, elapsedTime) {
+		if (data == null) {
+			return;
+		}
+		var xprediction = data.x; //these x coordinates are relative to the viewport
+		var yprediction = data.y; //these y coordinates are relative to the viewport
+		console.log(elapsedTime); //elapsed time is based on time since begin was called
+	}).begin();
 
-var prediction = webgazer.getCurrentPrediction();
-console.log(prediction);
-if (prediction) {
-    var x = prediction.x;
-    var y = prediction.y;
-}
-
-
+	var prediction = webgazer.getCurrentPrediction();
+	console.log(prediction);
+	if (prediction) {
+		var x = prediction.x;
+		var y = prediction.y;
+	}
 }	
