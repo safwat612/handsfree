47f1177d705956ea25400c9c3ebeac72ddf5e698
Safwat Musthafa <T00543609@mytru.ca>
2021-11-10 14:08:33 -0800

    hello

--- home.js
+++ home.js
@@ -55,6 +55,7 @@ function start() {
 		if (data == null) {
 			return;
 		}
+		console.log(data);
 		var xprediction = data.x; //these x coordinates are relative to the viewport
 		var yprediction = data.y; //these y coordinates are relative to the viewport
 		console.log(elapsedTime); //elapsed time is based on time since begin was called
